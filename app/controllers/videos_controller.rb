267ce9f013ac148dfdeb3d3c9b2fcce228e084cf
Kota Ishimoto <ishikota086@gmail.com>
2016-08-05 23:02:01 +0900

    Add video download method

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,4 +7,14 @@ class VideosController < ApplicationController
       render text: "404 Not found", status: 404
     end
   end
+
+  def download
+    @video = Video.find_by_video_id(params[:video_id])
+    if @video.present?
+      audio_path = File.join(Rails.root, 'tmp', 'cache', 'downloads', @video.video_id, "#{@video.video_id}.mp3")
+      send_file audio_path, :type => 'audio/mp3'
+    else
+      render text: "404 Not found", status: 404
+    end
+  end
 end
--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -15,4 +15,23 @@ describe 'Videos' do
       expect(response.body).to eq video.to_json
     end
   end
+
+  describe "GET /videos/download" do
+    let(:video) { FactoryGirl.create(:video) }
+
+    context "when file exists" do
+      it 'should send mp3 data' do
+        get "/videos/download/#{video.video_id}"
+        expect(response.body).to eq IO.binread(File.join(Rails.root, 'tmp', 'cache', 'downloads', video.video_id, "#{video.video_id}.mp3"))
+      end
+    end
+
+    context "when file not exists" do
+      it "should fail" do
+        get "/videos/download/hoge"
+        expect(response).not_to be_success
+        expect(response.status).to eq 404
+      end
+    end
+  end
 end
